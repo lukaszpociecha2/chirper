10aa8ccd41f8063507933115392b32cef7bb4200
Lukasz <lukasz.pociecha2@gmail.com>
2019-06-27 22:58:19 +0200

    CORS

--- src/main/java/pl/coderslab/chirper/controller/HomeControler.java
+++ src/main/java/pl/coderslab/chirper/controller/HomeControler.java
@@ -1,32 +1,31 @@
 package pl.coderslab.chirper.controller;
 
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.MediaType;
 import org.springframework.stereotype.Controller;
-import org.springframework.web.bind.annotation.CrossOrigin;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.ResponseBody;
+import org.springframework.web.bind.annotation.*;
 import pl.coderslab.chirper.entity.User;
 import pl.coderslab.chirper.repository.UserRepository;
 
-@Controller
+@RestController
 public class HomeControler {
 
     @Autowired
     private UserRepository userRepository;
 
-    @GetMapping("/")
-    @ResponseBody
-    @CrossOrigin //TODO configure CORS (default or open)
-    public String home(){
+    @GetMapping("/add-user")
+    @CrossOrigin
+    public User addUser(){
         User user = new User();
         user.setFirstName("Lukasz");
-        user.setLastName("Pociecha");
-        user.setEmail("lukasz@pociecha.com");
-        user.setPassword("password");
+        return user;
+    }
 
-        userRepository.save(user);
-
-        return "Welcome to chirper";
-
+    @PostMapping(value = "/add-user")
+    @CrossOrigin(methods = {RequestMethod.GET, RequestMethod.OPTIONS, RequestMethod.POST}) //TODO configure CORS (default or open)
+    public void home(@RequestBody String user){
+        System.out.println(user);
+        System.out.println("In HomeControl");
+        
     }
 }
--- src/main/java/pl/coderslab/chirper/sercurity/SecurityConfig.java
+++ src/main/java/pl/coderslab/chirper/sercurity/SecurityConfig.java
@@ -1,7 +1,9 @@
 package pl.coderslab.chirper.sercurity;
 
 import org.springframework.context.annotation.Configuration;
+import org.springframework.http.HttpMethod;
 import org.springframework.security.config.annotation.authentication.builders.AuthenticationManagerBuilder;
+import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
 import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter;
 
@@ -14,5 +16,10 @@ public class SecurityConfig extends WebSecurityConfigurerAdapter {
         auth.inMemoryAuthentication().withUser("user1").password("{noop}pass1").roles("USER");
     }
 
+    @Override
+    protected void configure(HttpSecurity http) throws Exception {
+        super.configure(http);
+        http.csrf().disable().authorizeRequests().anyRequest().permitAll().and().authorizeRequests().antMatchers(HttpMethod.POST).permitAll();
 
+    }
 }
